a2a9ada7b2d0531a447ab17317783167fa8f93cb
Donald P. Labajo Jr <ecedonaldlabajojr@gmail.com>
2021-01-19 00:49:54 +0800

    Added new page entry with back-end validation using express-validator

--- app.js
+++ app.js
@@ -4,41 +4,54 @@ const express = require('express');
 const ejs = require('ejs');
 const config = require('./config/database');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const {
     body,
     validationResult
 } = require('express-validator');
 
-// ========================== Initialize App ==========================
+
+// _______________________ Initialize App _______________________
 const app = express();
 
-
-// ========================== Set public folder ==========================
-app.use(express.static("public"));
-
-
-// ========================== Setup View Engine to EJS ==========================
-app.set('view engine', 'ejs');
-
-
-// ========================== Body- Middleware ==========================
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
-// ========================== Express Session ==========================
-app.set('trust proxy', 1) // trust first proxy
+app.use(cookieParser('secret'));
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: true
+        maxAge: null,
     }
-}))
+}));
 
-// ========================== Setup mongoose ODM ==========================
+// _______________________ Flash-message middleware _______________________
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+
+// _______________________ Setup View Engine to EJS _______________________
+app.set('view engine', 'ejs');
+
+
+
+// _______________________ Set Router _______________________
+const pages = require('./routes/pages');
+const adminPages = require('./routes/admin_pages');
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
+
+
+
+// _______________________ Setup mongoose ODM _______________________
 const mongoose = require('mongoose');
 mongoose.connect(config.database, config.dbOptions);
 
@@ -48,7 +61,9 @@ db.once('open', function () {
     console.log("We're connected to the database!");
 });
 
-// ========================== Setup MongoDB Schema & Model Using Mongoose ==========================
+
+
+// _______________________ Setup MongoDB Schema & Model Using Mongoose _______________________
 const itemSchema = mongoose.Schema({
     _id: {
         type: String,
@@ -68,15 +83,9 @@ const Item = new mongoose.model("Item", itemSchema);
 
 
 
-// ========================== Set Router ==========================
-const pages = require('./routes/pages');
-const adminPages = require('./routes/admin_pages');
-
-app.use('/', pages);
-app.use('/admin/pages', adminPages);
 
 
-// ========================== Setup Express-Validator on POST request ==========================
+// _______________________ Setup Express-Validator on POST request _______________________
 app.post(
     '/user',
     // username must be an email
@@ -100,32 +109,6 @@ app.post(
         }).then(user => res.json(user));
     },
 );
-
-// // Express Messages Middleware
-// app.use(require('connect-flash')());
-// app.use(function (req, res, next) {
-//     res.locals.messages = require('express-messages')(req, res);
-//     next();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,22 +1,102 @@
 const express = require('express');
 const router = express.Router();
+const {
+    body,
+    validationResult
+} = require('express-validator');
 
+
+// Get Pages Model
+const Page = require("../models/page");
+
+
+
+
+// Routes
 router.get('/', (req, res) => {
     res.render("index", {
-        title: "ADMIN PAGE",
+        title: "Admin Page",
         content: "Admin Page"
     });
 });
 
 router.get('/add-pages', (req, res) => {
-    const title = "";
-    const slug = "";
-    const content = "";
-    res.render("admin/add_pages", {
-        title: title,
-        slug: slug,
-        content: content
-    })
+    res.render("admin/add_pages")
 })
 
+// router.post('/add-pages', [
+//         body('title').not().isEmpty().withMessage("Please provided a title."),
+//         body('content', "Content cannot be empty.").not().isEmpty(),
+//     ],
+//     (req, res) => {
+
+//         // Response Object containing {1}Status and {2}Success/Fail array
+
+//         // Validate Form for errors
+//         const errors = validationResult(req);
+
+//         // If Slug value is empty, place page Title instead
+
+//         if (!errors.isEmpty()) {} else {}
+//         res.redirect('/add-pages');
+//     });
+
+router.post('/add-pages', [
+    body('title').not().isEmpty().withMessage("Please provided a title."),
+    body('content', "Content cannot be empty.").not().isEmpty(),
+], (req, res) => {
+    console.log(req.body);
+    // Validate Form for errors
+    const errors = validationResult(req);
+
+    //Contains Errors
+    let errorArray = [];
+
+    // Check for errors from Form validation
+    errors.array().forEach((error) => {
+        errorArray.push(String(error.msg));
+    })
+
+    Page.findOne({
+        title: req.body.title
+    }, (err, foundPage) => {
+        if (errorArray.length === 0) {
+            if (!err) {
+                if (!foundPage) {
+                    let slugVal = req.body.slug;
+                    if (req.body.slug === "") {
+                        slugVal = req.body.title
+                    }
+                    let newPage = new Page({
+                        title: req.body.title,
+                        slug: slugVal,
+                        content: req.body.content
+                    });
+                    newPage.save();
+                    errorArray.push('Successfully added page.')
+                    setLocalsMsg('success', errorArray);
+                    res.redirect('/admin/pages/add-pages');
+                } else {
+                    errorArray.push("Page Title Already exists. Try again.");
+                    setLocalsMsg('danger', errorArray)
+                    res.redirect('/admin/pages/add-pages');
+                }
+            } else {
+                console.log(err);
+            }
+        } else {
+            setLocalsMsg('danger', errorArray)
+            res.redirect('/admin/pages/add-pages');
+        }
+    });
+
+    // Function that takes in 'success/danger' types for Bootstrap alert class,and errorArray to display on said alert component
+    function setLocalsMsg(type, errorArray) {
+        req.session.message = {
+            type: type,
+            errorArray: errorArray
+        }
+    }
+
+});
 module.exports = router;
